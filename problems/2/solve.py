b296dcf44f1d81e6cc5dedc375426276eee27aa5
alexsierra45 <alexsierraalcala@gmail.com>
2024-09-20 16:19:11 -0400

    update second problem tester and solution

--- problems/2/solve.py
+++ problems/2/solve.py
@@ -1,5 +1,62 @@
-def solve(A,B,p,k):
-    n=max(max(A),max(B))+1
+def solve(A, B, p, k):
+    n = max(max(A), max(B)) + 1
+
+    # mask_A = [0] * n
+    # mask_B = [0] * n
+    # mask = [0] * n
+
+    # for problem in A:
+    #     mask_A[problem] |= 1
+    # for problem in B:
+    #     mask_B[problem] |= 1
+    # for i in range(n):
+    #     mask[i] = mask_A[i] & mask_B[i]
+
+    # solved_A = [0] * n
+    # solved_B = [0] * n
+    # solved_AND = [0] * n
+    # for i in range(0, n):
+    #     solved_A[i] += solved_A[i - 1] + mask_A[i]
+    #     solved_B[i] += solved_B[i - 1] + mask_B[i]
+    #     solved_AND[i] += solved_AND[i - 1] + mask[i]
+
+    # cache = dict()
+
+    # def dynamic_solve(index, a, b, current_p):
+    #     # Visited previously
+    #     if (index, a, b, current_p) in cache:
+    #         return cache[index, a, b, current_p]
+        
+    #     # Base case
+    #     if index == n:
+    #         cache[index, a, b, current_p] = 0
+    #         return 0
+        
+    #     # Ignore problem
+    #     possible_solutions = [
+    #         dynamic_solve(index + 1, max(a - 1, 0), max(b - 1, 0), current_p)
+    #     ]
+
+    #     # Take problem in A
+    #     if current_p > 0 and a == 0 and mask_A[index]:
+    #         possible_solutions.append(
+    #             dynamic_solve(index + 1, k - 1, max(b - 1, 0), current_p - 1)
+    #             + solved_A[min(index + k - 1, n - 1)] - (0 if index == 0 else solved_A[index - 1])
+    #             - (0 if b == 0 else (solved_AND[min(index + b - 1, n - 1)] - (0 if index == 0 else solved_AND[index - 1])))
+    #         )
+
+    #     # Take problem in B
+    #     if current_p > 0 and b == 0 and mask_B[index]:
+    #         possible_solutions.append(
+    #             dynamic_solve(index + 1, max(a - 1, 0), k - 1, current_p - 1)
+    #             + solved_B[min(index + k - 1, n - 1)] - (0 if index == 0 else solved_B[index - 1])
+    #             - (0 if a == 0 else (solved_AND[min(index + a - 1, n - 1)] - (0 if index == 0 else solved_AND[index - 1])))
+    #         )
+
+    #     cache[index, a, b, current_p] = max(possible_solutions)
+
+    #     return cache[index, a, b, current_p]
+
     arr=[]
     for i in range(n):
         arr.append(0)
@@ -41,4 +98,4 @@ def solve(A,B,p,k):
 
         return cache[i,k_A,k_B,current_p]
     
-    return dynamic_solve(0,0,0,p)
+    return dynamic_solve(0, 0, 0, p)
--- problems/2/test.py
+++ problems/2/test.py
@@ -1,51 +1,80 @@
 import random
 import itertools
-import solve
+from solve import solve
 
-MAX_N=20
-MAX_P=5
-MAX_K=6
+MAX_N = 25
+MAX_P = 5
+MAX_K = 6
 
-def build_test():
+def generate_case():
     global MAX_N
     global MAX_P
     global MAX_K
 
-    A=[i for i in range(MAX_N) if random.randint(0,1)==1]
-    B=[i for i in range(MAX_N) if random.randint(0,1)==1]
+    A=[i for i in range(MAX_N) if random.randint(0, 1) == 1]
+    B=[i for i in range(MAX_N) if random.randint(0, 1) == 1]
     
-    p=random.randint(1,MAX_P)
-    k=random.randint(1,MAX_K)
+    p = random.randint(1, MAX_P)
+    k = random.randint(1, MAX_K)
 
-    return (A,B,p,k)
-
-def get_solutions(A,B,p):
-    if len(A)+len(B)<=p:
-        return [(a,'A') for a in A]+[(b,'B') for b in B]
-    return list(itertools.combinations(
-        [(a,'A') for a in A]+[(b,'B') for b in B], p)
-    )
-
-def try_solve(solution, A, B, k):
-    answers=set()
-    for k_i in range(k):
-        for a_i, char in solution:
-            if a_i+k_i in (A if char=='A' else B):
-                answers.add(a_i+k_i)
-    return len(answers)
+    if len(A) == 0 or len(B) == 0:
+        return generate_case()
+    return (A, B, p, k)
 
 def solve_test(A, B, p, k):
-    best=0
-    for solution in get_solutions(A.copy(),B.copy(),p):
-        best=max(try_solve(solution,A,B,k),best)
+    best = 0
+
+    if len(A) + len(B) <= p:
+        ans = set()
+        for a in A:
+            ans.add(a)
+        for b in B:
+            ans.add(b)
+
+        return len(ans)
+
+    def get_combinations():
+        return list(itertools.combinations(
+            [(a, 'A') for a in A] + [(b, 'B') for b in B], p))
+
+    def try_solve(solution):
+        answers = set()
+        for k_i in range(k):
+            try:
+                for a_i, char in solution:
+                    if a_i+k_i in (A if char == 'A' else B):
+                        answers.add(a_i + k_i)
+            except:
+                a_i, char = solution
+                if a_i+k_i in (A if char == 'A' else B):
+                    answers.add(a_i + k_i)
+
+        return len(answers)
+    
+    for solution in get_combinations():
+        try:
+            best = max(try_solve(solution), best)
+        except:
+            print(solution)
+        best = max(try_solve(solution), best)
+
     return best
 
-for i in range(100):
-    test=build_test()
-    fb=solve_test(test[0],test[1],test[2],test[3])
-    ds=solve.solve(test[0],test[1],test[2],test[3])
-    if fb!=ds:
-        print("Error en test ",i)
-        print(test)
-        print(fb,ds)
-        break
+def generate_test(num):
+    tests = []
+    for _ in range(num):
+        case = generate_case()
+        tests.append((case, solve_test(case[0], case[1], case[2], case[3])))
+    accuracy = 0
+    for case, ans in tests:
+        solution = solve(case[0], case[1], case[2], case[3])
+        if solution == ans:
+            accuracy += 1
+        else:
+            print(f'Error in test {case}')
+            print(f'Solution: {solution}, expected solution: {ans}')
+
+    plural = '' if num == 1 else 's'
+    print(f'Accuracy of {accuracy / num * 100}% for {num} test{plural} case{plural}')
+
+generate_test(100)
